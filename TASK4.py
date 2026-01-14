280b52962d6ef0c12edce37e9730e1536bd295d8
Sishir <application.sishir@gmail.com>
2026-01-14 12:03:45 +0530

    Add files via upload

---
+++ TASK4.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total_sum = 0
+
+# Iterate from 1 to 50 using a for loop
+for i in range(1, 51):
+    total_sum += i
+
+# Display the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
